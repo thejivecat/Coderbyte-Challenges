4edb07375070bfb9410057ea751bdb4b50f73118
Srujan Bethi <ssbethi@gmail.com>
2019-12-09 10:32:12 -0700

    deleted files

--- WordCount.js
+++
@@ -1,8 +0,0 @@
-// Have the function WordCount(str) take the str string parameter being passed and return the 
-// number of words the string contains (e.g. "Never eat shredded wheat or cake" would return 6). 
-// Words will be separated by single spaces.
-
-function WordCount(str) { 
-  let words = str.split(' ');
-  return words.length;
-}
