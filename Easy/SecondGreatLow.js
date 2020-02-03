0cdfa4a9fc057e2830eddd1cf3f79319e25a65e2
Srujan Bethi <ssbethi@gmail.com>
2020-02-03 11:37:33 -0700

    algorithm to find second lowest and second greatest numbers in an array

---
+++ Easy/SecondGreatLow.js
@@ -0,0 +1,25 @@
+// Have the function SecondGreatLow(arr) take the array of numbers 
+// stored in arr and return the second lowest and second greatest 
+// numbers, respectively, separated by a space. For example: if arr 
+// contains [7, 7, 12, 98, 106] the output should be 12 98. The array 
+// will not be empty and will contain at least 2 numbers. It can get 
+// tricky if there's just two numbers!
+
+function SecondGreatLow(arr) { 
+  const min = Math.min(...arr);
+  const max = Math.max(...arr);
+  let secondGreat = min;
+  let secondLow = max;
+  for (let i = 0; i < arr.length; i++) {
+    let num = arr[i];
+    if (num < max && num > secondGreat) {
+      secondGreat = num;
+    } 
+    if (num > min && num < secondLow) {
+      secondLow = num;
+    }
+  }
+  return `${secondLow} ${secondGreat}`
+}
+
+console.log(SecondGreatLow([80, 80]))
