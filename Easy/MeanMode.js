bdbbf5877452b09295379ea7a8359be4281d7124
Srujan Bethi <ssbethi@gmail.com>
2020-02-10 11:53:52 -0700

    tests passing

---
+++ Easy/MeanMode.js
@@ -0,0 +1,21 @@
+function MeanMode(arr) { 
+  //mean is average
+  //mode is most repeated number
+  let mean = (arr.reduce((acc, cur) => acc + cur))/(arr.length);
+  let counter = {};
+  for (let i = 0; i < arr.length; i++) {
+    counter[arr[i]] = counter[arr[i]] + 1 || 0;
+  }
+  let mode = Object.keys(counter).reduce((a, b) => counter[a] > counter[b] ? a : b);
+  console.log(mean, 'mean');
+  console.log(mode, 'mode', typeof mode, 'typeof');
+  if (mean == mode) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+const test1 = [4, 4, 4, 6, 2];
+const test2 = [10, 10];
+console.log(MeanMode(test2));
